59ed9e76c284d300cb18a3a514c059f30e8553e3
root <bright.cxc@gmail.com>
2022-03-10 13:59:15 -0800

    added sizeof function to make program dynamic

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - prints to std output sizes of various types
+ * sizeof - calculates the number of bytes of type passed
  * Return: Zero if code runs without error
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", 1);
-	printf("Size of an int: %d byte(s)\n", 4);
-	printf("Size of a long int: %d byte(s)\n", 8);
-	printf("Size of a long long int: %d byte(s)\n", 8);
-	printf("Size of a float: %d byte(s)\n", 4);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
 	return (0);
 }
