1d05793b3485ebb03f1195e1f61b046fde4ebf7d
root <bright.cxc@gmail.com>
2022-03-10 13:14:24 -0800

    updated script to fit betty coding style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/*
+ * 
+ * prints to standard output using function puts
+ */
+
+int main(void)
+{
 	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
