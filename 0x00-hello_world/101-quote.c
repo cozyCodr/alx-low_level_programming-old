74c7bfe918ced82621323dfa5489cd06a297482c
root <bright.cxc@gmail.com>
2022-03-10 14:24:23 -0800

    created script 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints quote to output
+ * fwrite - writes passed string to standard output
+ * Return: One if successful
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
