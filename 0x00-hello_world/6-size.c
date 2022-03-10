5d2fc94a649450449420844557a37f2c07c2b41f
root <bright.cxc@gmail.com>
2022-03-10 13:52:22 -0800

    created script

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints to std output sizes of various types
+ * Return: Zero if code runs without error
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", 1);
+	printf("Size of an int: %d byte(s)\n", 4);
+	printf("Size of a long int: %d byte(s)\n", 8);
+	printf("Size of a long long int: %d byte(s)\n", 8);
+	printf("Size of a float: %d byte(s)\n", 4);
+	return (0);
+}
