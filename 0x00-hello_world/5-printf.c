9135c4097fdf412bb68399cc278bd9c15e26ddbe
root <bright.cxc@gmail.com>
2022-03-10 13:37:34 -0800

    created 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints to std output usinf printf
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
