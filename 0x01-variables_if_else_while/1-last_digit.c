ed1735bc61cc089edd558a6bc835752234a9747e
root <bright.cxc@gmail.com>
2022-03-11 12:51:13 -0800

    changed lastd declaration position

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 	int n;
+	int lastd = n % 10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if ((n % 10) > 5)
+	if (lastd > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
-	else if ((n % 10) < 6 && (n % 10) != 0)
+	else if (lastd < 6 && lastd != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 	else 
 		printf("Last digit of %d is %d and is 0\n", n, lastd);
