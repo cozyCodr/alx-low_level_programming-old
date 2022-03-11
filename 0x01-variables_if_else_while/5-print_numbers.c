0a2561f7d3c2e4a32e3f7b97ade51a147ac3b127
root <bright.cxc@gmail.com>
2022-03-11 13:48:13 -0800

    created 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers less that 10
+ * Return: Zero if code runs without exception
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 0; i < 10; i = i + 1)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
