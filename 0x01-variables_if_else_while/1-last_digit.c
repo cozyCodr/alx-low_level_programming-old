cfe32767ff9be8917bbfe0105ec462ccd7102c91
root <bright.cxc@gmail.com>
2022-03-11 12:59:36 -0800

    removed trailing whitespace from lines 5,8 and 24

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <time.h>
 /* more headers goes there */
 #include <stdio.h>
-/** 
+/**
 * main - prints the last digit in a number
 * lastd - stores the last digit variable
-* Return: 0 if code runs without exception 
+* Return: 0 if code runs without exception
 */
 int main(void)
 {
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
 	else if (lastd < 6 && lastd != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
-	else 
+	else
 		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	return (0);
 }
