d83af0facc8828dbcbc7aab67b3eed7d67845fbc
root <bright.cxc@gmail.com>
2022-03-11 15:11:15 -0800

    created 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints all hex numbers
+ * Return: zero if code runs without exception
+ */
+int main(void)
+{
+	int c;
+
+	c = 0x30;
+	while (c < 0x40)
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	c = 0x61;
+	while (c < 0x67)
+	{
+		putchar(c);
+		c = c + 1;
+	}
+	putchar('\n');
+	return (0);
+}
