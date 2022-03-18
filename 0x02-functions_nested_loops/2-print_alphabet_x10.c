714941b056f5c3135106ed606da09c4ca4bc1662
root <bright.cxc@gmail.com>
2022-03-18 15:18:45 -0700

    created 2-print_alphabet_x10.c and added its signature to main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  print_alphabet_x10 - prints the letters a to z in 10 rows
+ */
+void print_alphabet_x10(void)
+{
+	int start = 0;
+
+	while(start < 10){
+		print_alphabet();
+	}
+}
