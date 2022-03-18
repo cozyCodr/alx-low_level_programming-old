25620ccde87fc261bc76919243f372d6238d34a0
root <bright.cxc@gmail.com>
2022-03-18 14:35:05 -0700

    we try again

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef myheader
 #define myheader
 #include <unistd.h>
-ssize_t write(int f, const void *buf, size_t nbyte);
+int _putchar(char c);
+
 int _putchar(char c){
 	return (write(1, &c, 1));
 }
