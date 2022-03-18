3ac9404b695a5368d33bc07a6c47497c753ff745
root <bright.cxc@gmail.com>
2022-03-18 15:41:23 -0700

    created 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower - returns 1 if lowercase else returns 0
+ */
+int _islower(int c){
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else {
+		return (0);
+	}
+}
