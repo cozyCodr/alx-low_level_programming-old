fc4f2768443aa76347bb947c33fa0205fe2b0f85
root <bright.cxc@gmail.com>
2022-03-18 16:23:53 -0700

    created 4-isalpha.c and added prototype to main.h

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - returns 1 if alphabet letter else returns 0
+ * @c: parametre tested if part of alphabet or not
+ * Return: 1 if true, 0 if false
+ */
+int _isalpha(int c)
+{
+	if ((c > 96 && c < 123) || (c > 47 && < 58))
+		return (1);
+	else
+		return (0);
+}
