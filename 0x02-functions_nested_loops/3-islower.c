81f0e594578379bebeb65a86167f06297155dd39
root <bright.cxc@gmail.com>
2022-03-18 15:46:32 -0700

    refactored to match betty coding sstyle

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * _islower - returns 1 if lowercase else returns 0
+ * @c: parametre tested if lowercase or not
+ * Return: 1 if true, 0 if false
  */
-int _islower(int c){
+int _islower(int c)
+{
 	if (c > 96 && c < 123)
-	{
 		return (1);
-	}
-	else {
+	else
 		return (0);
-	}
 }
