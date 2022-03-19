1b95b13ced56ec87afaf751e28cb78674ece8cf8
root <bright.cxc@gmail.com>
2022-03-19 07:51:39 -0700

    created 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit in number
+ * @c: param to check last number
+ * Return: the last digit of a number
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
