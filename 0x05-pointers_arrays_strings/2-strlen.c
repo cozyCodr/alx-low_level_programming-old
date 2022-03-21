5c601619be8d5318e57a4423e6750b45a0344866
root <bright.cxc@gmail.com>
2022-03-21 03:55:38 -0700

    modified return value

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- *
- *
+ * _strlen - finds length pf string
+ * @s: subject string
+ * Return: string length
  */
 int _strlen(char *s)
 {
 	int count;
-	char x = *s;
+	char *x = s;
 
 	count = 0;
-	while ( x != '\0')
+	while (x != '\0')
 	{
 		count += 1;
-		x = (*(x + count));
+		x = x + 1;
 	}
-	return (count);
+	return (count - x);
 }
