e461138cb27dea3585b264f7184e158ee844f8c6
root <bright.cxc@gmail.com>
2022-03-22 16:41:38 -0700

    more refactoring

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts-half - prints half of the sreing starting from the middle
+ * puts_half - prints half of the sreing starting from the middle
  * @str: subject string
  * Return: Nothing
  */
@@ -9,13 +9,18 @@ void puts_half(char *str)
 	int len;
 	int half;
 
-	len = _strlen(str);
+	len = 0;
+	while (*str != '\0')
+	{
+		str += 1;
+		len += 1;
+	}
 	if ((len % 2) == 0)
 		half = len / 2;
 	else 
 		half = (len - 1) / 2;
 
-	while (*(str + half) != '\0')
+	while (str[half] != '\0')
 	{
 		_putchar(*str);
 		str = str + 1;
