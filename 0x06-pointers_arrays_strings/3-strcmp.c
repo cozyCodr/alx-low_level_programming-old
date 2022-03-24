2b4d6a8860b0f5fcfd20cb5b5e3d57d75b7a0e63
root <bright.cxc@gmail.com>
2022-03-24 04:22:11 -0700

    created string compare function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ * Return: result of comparison
+ */
+int _strcmp(char *s1, char *s2){
+	while (*s1)
+	{
+		if (*s1 != *s2)
+			break;
+		s1++;
+		s2++;
+	}
+	return (*unsigned char*) s1 - *(unsigned char*) s2;
+}
