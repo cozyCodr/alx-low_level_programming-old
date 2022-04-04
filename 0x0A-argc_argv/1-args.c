b5d3f88557b3bfa00fe7ac2057460a725d8aee68
Bright Londa <bright.cxc@gmail.com>
2022-04-04 12:28:57 +0200

    created number of arguments printing programme

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints number of command line arguments
+ * @argc: number of arguments
+ * @argv: array of string arguments
+ * Return: zero if runs without errors
+ */
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d", argc);
+	putchar('\n');
+	return (0);
+}
