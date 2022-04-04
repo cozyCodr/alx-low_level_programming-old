0f2b37557fec4d8f56af460f0cf747360c6b73f4
Bright Londa <bright.cxc@gmail.com>
2022-04-04 12:19:05 +0200

    refactored to betty coding style

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
-/** main - prints the name of the program
+/**
+ * main - prints the name of the program
  * @argc: number of arguments passed
  * @argv: array of string arguments
  * Return: 0 if run without errors
