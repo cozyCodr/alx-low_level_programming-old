9555213f629a487da8ed38e61d5efe735761cad1
Bright Londa <bright.cxc@gmail.com>
2022-04-04 11:49:32 +0200

    created program that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/** main - prints the name of the program
+ * @argc: number of arguments passed
+ * @argv: array of string arguments
+ * Return: 0 if run without errors
+ */
+int main(int argc **argv)
+{
+	printf(argv[0]);
+	_putchar('\n');
+	return (0);
+}
