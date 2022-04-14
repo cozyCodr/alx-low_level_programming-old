2d94502f0cfb265bc7bdb3751c8d6d524413911b
Bright Londa <bright.cxc@gmail.com>
2022-04-14 21:03:37 +0200

    added NULL params guard

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,8 @@
 #include "function_pointers.h"
 #include <stddef.h>
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
 /**
  * array_iterator - uses passed function pointer to print each item in arry
  * @array: array containing items to be printed
@@ -11,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != ((void *)0))
+	if (action != NULL || array != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
