2a01ec71df2a2fc924a1d2a7bd25b1b4a518c608
Bright Londa <bright.cxc@gmail.com>
2022-04-14 19:54:37 +0200

    patch 2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,7 @@
 #include "function_pointers.h"
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
 /**
  * print_name - uses function pointed to by pointer to print name
  * @name: name to be printed
@@ -7,6 +10,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != ((void*)0))
+	if (f != NULL)
 		f(name);
 }
