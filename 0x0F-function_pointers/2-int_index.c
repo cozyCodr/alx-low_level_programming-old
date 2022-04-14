3d98ff627c9a5abf18afc29dc62cd8eb967db05f
Bright Londa <bright.cxc@gmail.com>
2022-04-14 21:27:39 +0200

    updated NULL guards

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,11 +2,18 @@
 #ifndef NULL
 #define NULL ((void *)0)
 #endif
+/**
+ * int_index - finds and prints first int in array
+ * @array: array to  
+ * @size: size of array
+ * @cmp: function that compares ints
+ * Return: first int found or -1 
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array != NULL || cmp != NULL)
+	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
