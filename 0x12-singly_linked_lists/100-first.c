6c5fd7cdc53ccb4905d34bbd974ceb35b70fd98e
Bright Londa <bright.cxc@gmail.com>
2022-04-22 15:08:05 +0200

    refactored to betty coding style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * myStartupFun - Print text before function start 
+ * myStartupFun - Print text before function start
  * Return: nothing
  */
-void myStartupFun (void)
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf(" I bore my house upon my back!\n");
 }
