02ab91bc2fab7c8de531198ec13061b924c6443f
Bright Londa <bright.cxc@gmail.com>
2022-04-23 05:27:25 +0200

    created node content printing function

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - rprints elements of the list
+ * @h: pointer to address of a first item on list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+	}
+
+	return (count);
+}
