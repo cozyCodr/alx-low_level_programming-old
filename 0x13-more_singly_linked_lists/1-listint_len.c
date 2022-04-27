8292554bbe2dcaf9e414442b7f2a4fa9f8eb14e0
Bright Londa <bright.cxc@gmail.com>
2022-04-27 02:01:09 +0200

    created node counter

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_listint - returns number of elements in a list
+ * @h: head of list
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
