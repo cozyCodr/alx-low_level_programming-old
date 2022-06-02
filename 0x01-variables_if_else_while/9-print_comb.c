658c52124d4b95ceddc03d04c104ecfe682766c4
Bright Londa <bright.cxc@gmail.com>
2022-06-02 14:35:34 +0200

    first commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints single digit combinations
+ * Return: zero if code runs without exception
+ */
+int main(void)
+{
+	for (int c = 48; c < 58; c++)
+	{
+		putchar(c);
+		if (c != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
