b662e50816e60f8198ef3754dcb6cf1e349cff61
cozyCodr <bright@BullzEye007.localdomain>
2022-06-12 12:19:38 +0200

    added 100-times-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  print_times_table - prints an n multiplication table
+ *  @n: n X n time table
+ *  Return: Noting
+ */
+void print_times_table(int n)
+{
+	int x;
+	int y;
+	int result;
+	char *res;
+	char temp[5];
+
+	if (n <= 15 || n > 0)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
+			{
+				result = x * y;
+				sprintf(temp, "%d", result);
+				res = temp;
+				if (y == n)
+				{
+					printf("%4s", res);
+					_putchar('\n')
+				}
+				else
+					printf("%4s,", res);
+			}
+		}
+	}
+}
