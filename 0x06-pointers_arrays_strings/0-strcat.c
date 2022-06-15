a5f18352298d9edf840ad652255713bdb815f6ee
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-06-15 08:30:17 +0000

    intermmediate repair

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
+
+/**
+ * _copyString - copys a string to a different memory location
+ * @s: String to be copied
+ * Return: string copied to a new memory location
+ */
+char  _copyString(char s[])
+{
+	int i;
+	char *s2;
+
+	s2 = (char *)malloc(20);
+
+	for (i = 0; s[i] != '\0'; i++)
+	s2[i] = s[i];
+
+	return ((char *)s2);
+}
 /**
  * _strcat - concatenates 2 strings
  * @dest: destination string
@@ -8,40 +26,24 @@
  */
 char *_strcat(char *dest, char *src)
 {
-        int dest_len;
-        char *dest_copy;
+	int dest_len;
+	char *dest_copy;
 
-        dest_copy = _copyString(dest);
-        dest_len = 0;
+	dest_copy = _copyString(dest);
+	dest_len = 0;
 
-        while (*dest_copy != '\0')
-        {   
-                dest_len += 1;
-                dest_copy += 1;
-        }   
+	while (*dest_copy != '\0')
+	{
+		dest_len += 1;
+		dest_copy += 1;
+	}
 
-        while (*src != '\0')
-        {   
-                dest[dest_len] = *src;
-                dest_len++;
-                src++;
-        }   
-        return dest;
+	while (*src != '\0')
+	{
+		dest[dest_len] = *src;
+		dest_len++;
+		src++;
+	}
+	return (dest);
 }
-/**
- * _copyString - copys a string to a different memory location
- * @s: String to be copied
- * Return: string copied to a new memory location
- */
-char* _copyString(char s[])
-{
-        int i;
-        char* s2;
 
-        s2 = (char*)malloc(20);
-
-        for (i = 0; s[i] != '\0'; i++)
-                s2[i] = s[i];
-
-        return ((char*)s2);
-}
