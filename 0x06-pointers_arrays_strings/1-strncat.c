1c0af22e556a0b57bcb4ae51fc51b5f9695c7274
cozyCodr <brightlonda.scholar@gmail.com>
2022-06-15 11:05:05 +0200

    fixed errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * _copyString - copys a string to a different memory location
+ * @s: String to be copied
+ * Return: copied string
+ */
+char *_copyString(char s[])
+{
+	int i;
+	char *s2;
+
+	s2 = (char *)malloc(20);
+
+	for (i = 0; s[i] != '\0'; i++)
+	s2[i] = s[i];
+
+	return ((char *)s2);
+}
+/**
  * _strncat - concatenates 2 strings
  * @dest: destination string
  * @src: source string
@@ -26,22 +43,5 @@ char *_strncat(char *dest, char *src, int n)
 		dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
 
-	return dest;
-}
-/**
- * _copyString - copys a string to a different memory location
- * @s: String to be copied
- * Return: string copied to a new memory location
- */
-char* _copyString(char s[])
-{
-        int i;
-        char* s2;
-
-        s2 = (char*)malloc(20);
-
-        for (i = 0; s[i] != '\0'; i++)
-                s2[i] = s[i];
-
-        return ((char*)s2);
+	return (dest);
 }
