fb6c8e2f5d887fc95839cb3aa4639b11117d7e7e
Bright Londa <66696421+cozyCodr@users.noreply.github.com>
2022-06-16 11:13:38 +0200

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,11 @@ int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void print_times_table(int n);
+void print_alphabet(void);
+void fibonacci(void);
+int print_sign(int n);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
 
 #endif
