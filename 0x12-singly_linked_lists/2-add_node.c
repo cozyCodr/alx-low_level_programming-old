aad4023eb373924473ea7a14ffb09cdc08905760
cozyCodr <brightlonda.scholar@gmail.com>
2022-07-01 16:15:32 +0200

    created add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - adds node to the start of the list
+ * @head: head of the list
+ * @str: string field of struct list_s
+ * Return: head of node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+
+	if (*head == NULL && str != NULL)
+	{
+		*head = malloc(sizeof(list_t));
+		(*head)->str = strdup(str);
+		(*head)->next = NULL;
+		return (*head);
+	}
+	else if (str == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+	temp->next = (*head);
+
+	(*head) = temp;
+
+	return (*head);
+
+}
